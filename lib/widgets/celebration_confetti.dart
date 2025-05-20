095c5ad4d97c9d5b90d8fc3d6900c3e03e71da9a
Fabian Baumert <contact@fabianbaumert.com>
2025-05-20 20:19:42 +0200

    feat: adjust confetti settings for improved celebration effect

--- lib/widgets/celebration_confetti.dart
+++ lib/widgets/celebration_confetti.dart
@@ -45,10 +45,10 @@ class _CelebrationConfettiState extends State<CelebrationConfetti> {
         confettiController: _controller,
         blastDirectionality: BlastDirectionality.explosive,
         shouldLoop: false,
-        numberOfParticles: 30,
+        numberOfParticles: 15, // Increased from 10
         maxBlastForce: 20,
         minBlastForce: 8,
-        emissionFrequency: 0.1,
+        emissionFrequency: 0.05, // Reduced from 0.1
         gravity: 0.3,
         colors: const [
           Colors.green,
